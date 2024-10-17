48f3111958490f0028d87092f4b238ec5bf55728
Matthew <52621525+H11Maitree@users.noreply.github.com>
2024-10-17 17:11:39 +0700

    Initial Commit

---
+++ iBeaconScannerTests/iBeaconScannerTests.swift
@@ -0,0 +1,17 @@
+//
+//  iBeaconScannerTests.swift
+//  iBeaconScannerTests
+//
+//  Created by Maitree Hirunteeyakul on 17/10/2024.
+//
+
+import Testing
+@testable import iBeaconScanner
+
+struct iBeaconScannerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
